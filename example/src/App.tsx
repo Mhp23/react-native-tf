6ad5b5cb7276bb3f43a531b3e22fe7898b45ca07
mhp23 <hpousty@gmail.com>
2024-01-24 18:15:39 +0330

    chore(example): cleanup App.tsx

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,20 +1,5 @@
 import * as React from 'react';
 
-import { StyleSheet, View } from 'react-native';
-
 export default function App() {
-  return <View style={styles.container} />;
+  return <></>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
-});
